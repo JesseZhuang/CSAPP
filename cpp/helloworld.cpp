c3ead877b027479bf1a47b77c6433f22f43b7fbc
Jesse Zhuang <zhzexi@gmail.com>
2023-01-02 21:38:39 -0800

    for loop comma 2 statements

--- cpp/helloworld.cpp
+++ cpp/helloworld.cpp
@@ -9,8 +9,6 @@ int main()
     vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
 
     for (const string &word : msg)
-    {
-        cout << word << " ";
-    }
+        cout << word, cout << " "; // multiple statements with comma on one line
     cout << endl;
 }
