a6d2fd8cef44e04d372ea3c1ae51f260458c5ea9
Jesse Zhuang <zhzexi@gmail.com>
2023-02-10 17:09:09 -0800

    lc 167

--- cpp/leetcode/src/array/TwoSumII.hpp
+++ cpp/leetcode/src/array/TwoSumII.hpp
@@ -8,8 +8,14 @@ using namespace std;
 // lc 167
 class Solution {
 public:
-    vector<int> twoSum(vector<int> &numbers, int target) {
-
+    vector<int> twoSum(vector<int> &numbers, int target) { // 12ms, 15.7Mb. two pointer.
+        for (int l = 0, r = numbers.size() - 1; l < r;) {
+            int add = numbers[l] + numbers[r];
+            if (add > target) r--;
+            else if (add < target) l++;
+            else return {l + 1, r + 1}; // iterator will need cast to const, troublesome
+        }
+        return {};
     }
 };
 
