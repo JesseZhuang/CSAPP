20caf05473d27cc36950d7c1db4b350563c410c5
Jesse Zhuang <zhzexi@gmail.com>
2024-10-26 20:14:26 -0700

    lc practice

---
+++ leetcode/src/dp/OrigTypedStringII.hpp
@@ -0,0 +1,43 @@
+#ifndef LEETCODE_ORIGTYPEDSTRINGII_HPP
+#define LEETCODE_ORIGTYPEDSTRINGII_HPP
+
+#include <vector>
+
+using namespace std;
+
+// dp[i][j] i: possibilities for character groups in w[0,i], string size at most j
+class Solution {
+public:
+    int possibleStringCount(string word, int k) {
+        const int M = 1e9 + 7;
+        size_t n = word.size();
+        long long res = 1;
+        vector<int> w;
+        size_t i = 0;
+        while (i < n) {
+            int ctr = 1;
+            while (i < n - 1 && word[i] == word[i + 1]) {
+                i++;
+                ctr++;
+            }
+            res = (res * ctr) % M;
+            w.push_back(ctr);
+            i++;
+        }
+        vector<long long> dp(k, 1), ndp(k);
+        // todo: why k+2?
+        for (int i = min(static_cast<int>(w.size()), k + 2) - 1; i >= 0; i--) {
+            vector<long long> pf(k + 1, 0);
+            for (int j = 0; j < k; j++) pf[j + 1] = (pf[j] + dp[j]) % M; // prefix sum
+            for (int rem = 0; rem < k; rem++) {
+                int x = min(w[i], rem);
+                ndp[rem] = (pf[rem] - pf[rem - x] + M) % M;
+            }
+            dp = ndp;
+        }
+        res = (res - dp[k - 1] + M) % M;
+        return static_cast<int>(res);
+    }
+};
+
+#endif //LEETCODE_ORIGTYPEDSTRINGII_HPP
