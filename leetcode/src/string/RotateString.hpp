aeb46b85c8f588b0b48f6c4aabb0bf41f4c68c44
Jesse Zhuang <zhzexi@gmail.com>
2024-11-02 21:11:50 -0700

    lc practice

--- leetcode/src/string/RotateString.hpp
+++ leetcode/src/string/RotateString.hpp
@@ -7,13 +7,23 @@
 
 using namespace std;
 
-class Solution {
+class Solution1 {
 public:
     bool rotateString(string s, string goal) {
         if (s.length() != goal.length()) return false;
         string doubleS = s + s;
         return KMP1D(goal).search(doubleS);
     }
+};
+
+class Solution2 {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.size() != goal.size()) return false; // Lengths must match for rotation
+        string combined = s + s; // Concatenate s with itself
+        return combined.find(goal) != string::npos;
+    }
+
 };
 
 #endif //LEETCODE_ROTATESTRING_HPP
